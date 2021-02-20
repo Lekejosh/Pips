c273fb8149dd7483196a458e56584d8a826659e3
lekejosh <lekejosh6wf@gmail.com>
2021-02-20 23:07:30 +0100

    --- Auto-Git Commit ---

--- source.py
+++ source.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route("/")
 def index():
-    return render_template("index.html") #render a template
+    return render_template("index.html")
 
 
 @app.route("/learn")
